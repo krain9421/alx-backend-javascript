7cc3acf4ac4eecddf464ef761d178b85217d79d7
Nwigwe Chidubem <krain9421@gmail.com>
2023-10-04 22:17:39 +0000

    commit

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,14 +7,35 @@ export interface Teacher {
   [propName: string]: any;
 }
 
-interface Directors extends Teacher {
+export interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-interface printTeacherFunction {
+export interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
   return `${firstName.charAt(0)}.${lastName}`;
+}
+
+export interface StudentClassConstructor {
+  new(firstName: string, lastName: string): Student;
+}
+
+export interface Student {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements Student {
+  constructor(public firstName: string, public lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
 }
