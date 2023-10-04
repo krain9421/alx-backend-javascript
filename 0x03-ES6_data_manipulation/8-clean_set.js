a9b0c15cb65d6ab241f4bb8158eea19c2f71399e
Nwigwe Chidubem <krain9421@gmail.com>
2023-10-04 06:52:40 +0000

    commit

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,9 @@
 export default function cleanSet(set, startString) {
   let result = '';
+
+  if (startString === '') {
+    return startString;
+  }
   for (const x of set) {
     if (x.startsWith(startString)) {
       result += `${x.slice(startString.length)}-`;
