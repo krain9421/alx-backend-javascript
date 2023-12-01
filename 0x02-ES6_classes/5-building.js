03b2b3b52dca64ab41a288ffae895eef02bd21ee
Nwigwe Chidubem <krain9421@gmail.com>
2023-12-01 18:55:58 +0000

    Added file for Task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,11 @@
 export default abstract class Building {
   constructor(sqft) {
+    if (!this.evacuationWarningMessage()) {
+      throw new Error("Class extending Building must override evacuationWarningMessage");
+    }
     this._sqft = sqft;
   }
-
+  
   // Getter
   get sqft() {
     return this._sqft;
