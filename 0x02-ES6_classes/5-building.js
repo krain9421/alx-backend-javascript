66bfcade1e6be0c4e3389bedf18370f45dfe3f3c
Nwigwe Chidubem <krain9421@gmail.com>
2023-12-02 02:33:48 +0000

    commit

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,12 +1,15 @@
 export default class Building {
   constructor(sqft = 0) {
     this._sqft = sqft;
-    if (this.constructor != Building && this.hasOwnProperty("evacuationWarningMessage") == false) {
-      throw new Error("Class extending Building must override evacuationWarningMessage");
+    if (this.constructor !== Building) {
+      this.evacuationWarningMessage();
     }
   }
-  
+ 
   // Method to be overwritten
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
 
   // Getter
   get sqft() {
