45ef18087ab2ca75cfd92fb880b37a759d9f3e9f
Nwigwe Chidubem <krain9421@gmail.com>
2023-12-02 03:05:31 +0000

    commit

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -11,7 +11,7 @@ export default class Building {
     return this._sqft;
   }
 
-  static evacuationWarningMessage() {
+  evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
