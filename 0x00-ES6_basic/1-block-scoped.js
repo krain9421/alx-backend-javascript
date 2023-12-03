77c482dcd1bdf558c7950582df72b4f01efec9a5
Nwigwe Chidubem <krain9421@gmail.com>
2023-12-03 12:31:25 +0000

    commit

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,14 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    let task = true;
+    let task2 = false;
+    if (task !== task2) {
+      task2 = true;
+      task = false;
+    }
   }
 
   return [task, task2];
