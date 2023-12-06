4880ae141eb341a53a20a8d3e6c3c83581cbba8f
Nwigwe Chidubem <krain9421@gmail.com>
2023-12-06 04:53:40 +0000

    commit

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,10 +4,10 @@ export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
     const value = weakMap.get(endpoint);
     weakMap.set(endpoint, value + 1);
-  }
-  else {
+  } else {
     weakMap.set(endpoint, 1);
   }
+
   if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
