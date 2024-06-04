bb320956d7fde01a023a7d684431af7877926e4f
Nwigwe Chidubem <krain9421@gmail.com>
2024-06-04 21:03:02 +0000

    Modified file

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,6 @@
 export default function hasValuesFromArray(set, array) {
-  for (const x of array) {
-    if (set.has(x) === false) {
-      return false;
-    }
+  if (new Set([...array.map((x) => set.has(x))]).has(false)) {
+    return false;
   }
   return true;
 }
