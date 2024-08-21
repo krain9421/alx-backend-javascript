b92e26d2ae20a0e9fadef75ddf229e0c76fa4d25
Nwigwe Chidubem <krain9421@gmail.com>
2024-08-21 01:10:41 +0100

    Task 6

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+const { express } = require('express');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
