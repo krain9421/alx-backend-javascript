d16290a4e1f7555bdb85e2e6a2ea2e64e15491ba
Nwigwe Chidubem <krain9421@gmail.com>
2024-09-22 08:45:48 +0100

    commit

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,12 +3,30 @@ const calculateNumber = require('./0-calcul.js');
 
 const numA = 10;
 const numB = 10;
-const isInteger = Number.isInteger(numA);
 var round = calculateNumber(numA, numB);
 
 describe("round test", function() {
-  it("checks rounded", function() {
-    const isInteger = Number.isInteger(numA)
-    assert(isInteger);
-  });
+    it('should return -2 when a is -1.4 and b is -0.6', () => {
+        assert.strictEqual(calculateNumber(-1.4, -0.6), -2);
+    });
+
+    it('should return 1 when a is 0.5 and b is 0.4', () => {
+        assert.strictEqual(calculateNumber(0.5, 0.4), 1);
+    });
+
+    it('should return 4 when a is 3.6 and b is 0.5', () => {
+        assert.strictEqual(calculateNumber(3.6, 0.5), 5);
+    });
+
+    it('should return -1 when a is -0.5 and b is -0.6', () => {
+        assert.strictEqual(calculateNumber(-0.5, -0.6), -1);
+    });
+
+    it('should return 2 when a is 1.5 and b is 0.5', () => {
+        assert.strictEqual(calculateNumber(1.5, 0.5), 3);
+    });
+
+    it('should return 3 when a is 2.5 and b is 0.5', () => {
+        assert.strictEqual(calculateNumber(2.5, 0.5), 4);
+    });
 });
